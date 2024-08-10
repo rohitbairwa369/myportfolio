dc99e1b96a81da3ee14e84c47e317ae76aed3743
Rohit  Bairwa <82049855+rohitbairwa369@users.noreply.github.com>
2024-08-10 23:10:53 +0530

    some changes

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const body = document.querySelector('body');
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+        body.classList.add(storedTheme); 
+        changebody()
+    }
+});
+
+
 const sidebar = document.querySelector('.sidebar');
 
 function showSidebar(){
   sidebar.style.display = "flex";
+  const nav = document.querySelector('nav');
+  nav.classList.remove('navblur');
 }
 
 function hideSidebar(){
@@ -37,4 +49,41 @@ var isNotViewed = true;
         }).observe(section);
 
 
-        
+window.addEventListener('scroll', function() {
+
+    let scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    const nav = document.querySelector('nav');
+    if (scrollPosition > 100) {
+        if(sidebar.style.display !='flex'){
+        nav.classList.add('navblur');
+        }
+    } else {
+        nav.classList.remove('navblur');
+    }
+});
+
+
+function changebody() {
+    const videoSource = document.querySelector('#videoplayer source');
+    const currentSrc = videoSource.getAttribute('src');
+    const body = document.querySelector('body');
+
+    // Define the two video sources
+    const source1 = 'https://reflect.app/home/build/q-c3d7becf.webm';
+    const source2 = './assets/rbwrodl.webm';
+
+    // Toggle between the sources
+    if (currentSrc === source1) {
+        videoSource.setAttribute('src', source2);
+        body.classList.add('dark');
+        window.localStorage.setItem('theme','dark')
+    } else {
+        videoSource.setAttribute('src', source1);
+        body.classList.remove('dark');
+        window.localStorage.clear()
+    }
+
+    // Reload the video with the new source
+    document.getElementById('videoplayer').load();
+}
+
